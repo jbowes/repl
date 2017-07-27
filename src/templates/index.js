97ca543ac1ab49ca04850927506ff2b9f249310a
James Bowes <jbowes@repl.ca>
2017-07-27 14:33:51 -0300

    different header for index vs post

--- src/templates/index.js
+++ src/templates/index.js
@@ -14,8 +14,14 @@ export default (props) => {
       <body className="near-black bg min-vh-100 flex flex-column helvetica">
         <header className="title bb bw3 mh5-ns mh3 flex">
           <div className="br bw3 flex-none ph4 pv3">
-            <h4 className="ttu mv0 fw4 tracked-mega">James Bowes</h4>
-            <h1 className="f-headline lh-solid ttu tracked-tight mv0">repl</h1>
+            <h1 className="ttu mv0 fw4 tracked-mega f6 pl2">James Bowes</h1>
+            <h1 className="f-subheadline lh-solid ttu tracked-tight mv0">repl</h1>
+          </div>
+          <div className="flex-auto pl5 pv3">
+            <h1 className="f-headline lh-solid ttu tracked mv0">website</h1>
+            <h1 className="f6 lh-solid ttu mv0 fw5 tracked-mega pl3">
+              1/128 scale realistic detail programming
+            </h1>
           </div>
         </header>
         <main className="w-90 w-two-thirds-ns center ph3-ns">
--- src/templates/post.js
+++ src/templates/post.js
@@ -12,11 +12,16 @@ export default (props) => {
       <Head />
       <body className="near-black bg min-vh-100 flex flex-column helvetica">
         <header className="title bb bw3 mh5-ns mh3 flex">
-          <div className="br bw3 flex-none ph4 pv3">
-            <h4 className="ttu mv0 fw4 tracked-mega">James Bowes</h4>
-            <h1 className="f-headline lh-solid ttu tracked-tight mv0">repl</h1>
+          <div className="br bw3 flex-none ph3 pv2">
+            <h1 className="f2 lh-solid ttu tracked-tight mv0">repl</h1>
+          </div>
+          <div className="flex-auto pl4 pv3">
+            <h1 className="f6 lh-solid ttu mv0 fw5 tracked-mega">
+              1/128 scale realistic detail programming
+            </h1>
           </div>
         </header>
+
         <main className="w-90 w-two-thirds-ns center ph3-ns">
             <Content title={props.title}>
                { props.children }
