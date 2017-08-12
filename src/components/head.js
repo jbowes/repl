b21d2badcc26d2e5e8b088bee5bece0d1f1f5fa0
James Bowes <jbowes@repl.ca>
2017-08-12 09:01:46 -0300

    Set theme-color for mobile

--- src/components/head.js
+++ src/components/head.js
@@ -12,6 +12,7 @@ export default (props) => {
         name="viewport"
         content="width=device-width, initial-scale=1.0"
       />
+      <meta name="theme-color" content="#2d4247" />
 
       <link rel="stylesheet" href="/css/style.css" />
 
