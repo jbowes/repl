3eeb25af74a8afee747c5065947deb6a3af0a04f
James Bowes <jbowes@repl.ca>
2020-03-15 11:11:26 -0300

    Switch analytics to gtag

--- src/components/analytics.js
+++ src/components/analytics.js
@@ -2,16 +2,15 @@ import React from 'react';
 
 export default (props) => {
   let code = `
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
 
-    ga('create', 'UA-103396477-1', 'auto');
-    ga('send', 'pageview');
+    gtag('config', 'UA-103396477-1');
   `;
 
   return (
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-103396477-1"/>
     <script dangerouslySetInnerHTML={ {__html: code }} />
   );
 };
