96092ea2c78f47ecbdff74373b369bf34dfb8f2a
James Bowes <jbowes@repl.ca>
2021-03-20 18:17:21 -0300

    Update structured-data.js

--- src/components/structured-data.js
+++ src/components/structured-data.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { JSONLD, Generic, Author } from 'react-structured-data';
 
 export default (props) => (
-  <JSONLD dangerouslyExposeHtml
+  <JSONLD dangerouslyExposeHtml>
       <Generic type="NewsArticle">
         <Author schema={{ name: 'James Bowes' }} />
       </Generic>
