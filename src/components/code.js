76f86407655364285b2d252db1ce84c412c19023
James Bowes <jbowes@repl.ca>
2021-03-28 12:37:38 -0300

    Update code.js

--- src/components/code.js
+++ src/components/code.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default (props) => {
   return (
-    <code className="dark-green">{ props.children }</code>
+    <code className="green">{ props.children }</code>
   );
 };
