167d69e25f9d8c3be3631b2042771b72e4370945
James Bowes <jbowes@repl.ca>
2021-03-28 11:04:32 -0300

    Update code.js

--- src/components/code.js
+++ src/components/code.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default (props) => {
   return (
-    <code className="beige dim bg-blue">{ props.children }</code>
+    <code className="beige bg-blue br4">{ props.children }</code>
   );
 };
