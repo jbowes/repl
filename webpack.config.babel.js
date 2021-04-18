a8ec0959f54189779b30c2456e7413525b975215
James Bowes <jbowes@repl.ca>
2021-04-18 07:49:25 -0300

    Update webpack.config.babel.js

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -47,7 +47,7 @@ export default {
         priority: 0.8,
       }, 
       paths: site.staticRoutes().filter(x => x !== '/').map(x => `/${x}/`).concat([ { path: '/', priority: 0.5 } ] ),
-    },
+    }),
     new webpack.DefinePlugin({ 'process.env': { 'NODE_ENV': JSON.stringify('production') } }),
     new CopyPlugin({
       patterns: [
